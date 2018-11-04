f495ddad819d785fdfeec3677aa1d694cfc49e2b
aquariuz <gpetiaa@gmail.com>
2018-11-04 23:19:11 +0200

    solved Session Handling and Authentication - Exercise

--- Session Handling and Authentication - Exercise/Models/BaseModel.php
+++ Session Handling and Authentication - Exercise/Models/BaseModel.php
@@ -8,9 +8,9 @@ abstract class BaseModel
     {
         if (self::$db == null) {
             self::$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-            self::$db->set_charset("utf8");
+            self::$db->set_charset('utf8');
             if (self::$db->connect_errno) {
-                die('Cannot connect to database');
+                die("Cannot connect to database");
             }
         }
     }
